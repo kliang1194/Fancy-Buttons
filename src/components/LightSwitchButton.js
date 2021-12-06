3623bc9539756bf8f20b06995d5e8dcb435b454b
Kyle <kliang1194@gmail.com>
2021-12-06 12:26:14 -0500

    Improve parent state modifications

--- src/components/LightSwitchButton.js
+++ src/components/LightSwitchButton.js
@@ -1,11 +1,12 @@
 import React, {useState} from 'react';
 
 export default function LightSwitchButton(props) {
-  const {light, setLight} = props;
-  const handleClick = () => props.setLight(props.light === "on" ? "off" : "on");
+  const {light, switchLight } = props;
+  const handleClick = () => switchLight();
+  
 
   return (
-    <button onClick={handleClick} className="LightSwitchButton">
+    <button onClick={props.switchLight} className="LightSwitchButton">
       {props.light === 'on' && <span><i>💡</i> I'm on!</span>}
       {props.light === 'off' && <span className="off"><i>💡</i> I'm off!</span>}
     </button>
