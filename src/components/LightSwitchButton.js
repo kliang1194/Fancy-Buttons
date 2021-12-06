3f6504ed3ae07460ff795d72f6449dbe2cf60fea
Kyle <kliang1194@gmail.com>
2021-12-06 12:13:57 -0500

    refactor state to lift state up

--- src/components/LightSwitchButton.js
+++ src/components/LightSwitchButton.js
@@ -1,13 +1,13 @@
 import React, {useState} from 'react';
 
 export default function LightSwitchButton(props) {
-  const [light, setLight] = useState('off');
-  const handleClick = () => setLight(light === "on" ? "off" : "on");
+  const {light, setLight} = props;
+  const handleClick = () => props.setLight(props.light === "on" ? "off" : "on");
 
   return (
     <button onClick={handleClick} className="LightSwitchButton">
-      {light === 'on' && <span><i>💡</i> I'm on!</span>}
-      {light === 'off' && <span className="off"><i>💡</i> I'm off!</span>}
+      {props.light === 'on' && <span><i>💡</i> I'm on!</span>}
+      {props.light === 'off' && <span className="off"><i>💡</i> I'm off!</span>}
     </button>
   );
 }
